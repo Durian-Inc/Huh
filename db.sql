6e4322311bedacd8d96a977949b991ecea4384bd
Jennifer Coleman <jkc266@mst.edu>
2018-10-13 00:44:48 -0500

    add images and db

---
+++ db.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Business{
+    FORMATTED_ADDRESS varchar(255),
+    FORMATTED_PHONE varchar(80),
+    ID varchar(255),
+    B_NAME varchar(255),
+    LATITUDE REAL,
+    LONGITUDE REAL,
+    PRIMARY KEY(ID),
+};
