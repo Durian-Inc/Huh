a02dc86078f4fdab00469a6b02cd4db498e91603
Jennifer Coleman <jkc266@mst.edu>
2018-10-13 13:13:50 -0500

    Added request function
    from lat & lng return name, place_id,lat,lng, and defaults as English language

--- app/views.py
+++ app/views.py
@@ -1,13 +1,27 @@
 from app import app
-from app.utils import query_places
-
+from app.utils import query_places, place_search
+from json import dumps
 
 @app.route('/')
 def index():
     return "index"
 
 
-@app.route('/query/')
+@app.route('/api/query/',methods=['GET'])
 def map_data():
-    return query_places()
+    results = place_search(38.682660,-90.282520)
+    locations = []
+    for result in results:
+        locations.append({
+            'name': result['name'],
+        #    # 'address': result['formatted_address'],
+        #     #'phone': result['formatted_phone'],
+        #     'type': result['type'],
+            'languages': 'English',
+            'lat': result['geometry']['location']['lat'],
+            'lng': result['geometry']['location']['lng'],
+            'place_id': result['place_id'],
+        })
+    return dumps(locations)
+
     
