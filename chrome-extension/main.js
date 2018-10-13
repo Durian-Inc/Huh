8a124d49bef10f9d3310e5267273ec0706dd5113
Connor J. Toth <tothconnor@gmail.com>
2018-10-13 06:32:03 -0500

    Added async code for creating profile using geolocation

--- chrome-extension/main.js
+++ chrome-extension/main.js
@@ -3,17 +3,63 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 
-
-/* Gets the user's current location */
-function getUserCurrentLocation() {
-
-  return Coordinates(5,2);
+var getUserCurrentLocation = function () {
+  var options = {maximumAge: 5 * 60 * 1000};
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject, options);
+  })
+  .then((position) => { return position.coords; })
+  .catch(onGeolocationError);
 }
 
-/* Calls backend with current location of the user
- * Returns a list of locations/events to display w/relevant information */
-function getListWithLocation(coordinates) {
+var getProfileForUser = function() {
+  return getUserCurrentLocation()
+    .then((coordinates) => {
+      return { coords: coordinates, };
+    });
+}
+
+
+/* Calls backend with profile of the current user
+   Returns a list of locations/events to display w/relevant information */
+var getListFromDatabaseUsingProfile = function(profile) {
 
   return Array();
-}
+};
 
+
+var formatListOfData = function(list) {
+  return false;
+};
+
+
+
+var onDocumentLoaded = function() {
+
+  /* determine if geolocation is supported; 
+     only continue if so */
+  var zipCodeProvided = false;
+  var enabled = navigator.geolocation || zipCodeProvided;
+  if (!enabled) {
+    return;
+  }
+
+  getProfileForUser()
+  .then(getListFromDatabaseUsingProfile)
+  .then(formatListOfData);
+};
+
+
+window.onload = onDocumentLoaded;
+
+function onGeolocationError(error) {
+  console.log('Error occurred. Error code: ' + error.code);
+  switch (error.code) {
+    case 0: console.log('\tUnknown error'); break;
+    case 1: console.log('\tPermission denied'); break;
+    case 2: console.log('\tPosition unavailable ' +
+        '(error response from location provider)'); 
+      break;
+    case 3: console.log('\tTimed out'); break;
+  }
+};
