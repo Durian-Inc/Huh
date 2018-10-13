4c25b755ea59aecb8d591cfa3739f7bd97c713f3
Connor J. Toth <tothconnor@gmail.com>
2018-10-13 12:39:59 -0500

    added permissions to storage; modified google template to perform our function

--- chrome-extension/options.js
+++ chrome-extension/options.js
@@ -2,9 +2,8 @@
 function saveOptions() {
   var zipCode = document.getElementById('zip').value;
   chrome.storage.sync.set({
-    zipCode: zipCode
+    zip: zipCode
   }, () => {
-    // Update status to let user know options were saved.
     var status = document.getElementById('status');
     status.textContent = 'Options saved.';
     setTimeout(() => {
@@ -13,18 +12,15 @@ function saveOptions() {
   });
 }
 
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
-function restore_options() {
-  // Use default value color = 'red' and likesColor = true.
+
+function restoreOptions() {
+  // default to no zip code provided
   chrome.storage.sync.get({
-    favoriteColor: 'red',
-    likesColor: true
-  }, function(items) {
-    document.getElementById('color').value = items.favoriteColor;
-    document.getElementById('like').checked = items.likesColor;
+    zip: ''
+  }, (items) => {
+    document.getElementById('zip').value = items.zip;
   });
 }
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click',
-    save_options);
+
+document.addEventListener('DOMContentLoaded', restoreOptions);
+document.getElementById('save').addEventListener('click', saveOptions);
