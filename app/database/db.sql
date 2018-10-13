1b10bd5b808fe84f4932a5fcaf3c71165f5c067e
Innocent Niyibizi <25cent9@gmail.com>
2018-10-13 11:48:08 -0500

    Fix Database Struture:
    Modified the location of the database sql file as well as changed a few names to make more sense.

---
+++ app/database/db.sql
@@ -0,0 +1,9 @@
+CREATE TABLE ExploreEntry{
+    FORMATTED_ADDRESS varchar(255),
+    FORMATTED_PHONE varchar(80),
+    ID varchar(255),
+    E_NAME varchar(255),
+    LATITUDE REAL,
+    LONGITUDE REAL,
+    PRIMARY KEY(ID)
+};
