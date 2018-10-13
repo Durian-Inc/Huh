c549ee439f49d4c1eaa31602fd3daa75a2413207
Connor J. Toth <tothconnor@gmail.com>
2018-10-13 15:26:28 -0500

    added new library

--- react-app/src/components/SearchbarContainer.js
+++ react-app/src/components/SearchbarContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import GoogleMapsLoader from "google-maps";
+import Autocomplete from 'react-autocomplete'
+//import GoogleMapsLoader from "google-maps";
 //import styled from "styled-components";
 
 /*const Map = styled.div`
@@ -11,6 +12,22 @@ class SearchbarContainer extends Component {
 
   render() {
     return <form>
+      <Autocomplete
+        getItemValue={(item) => item.label}
+        items={[
+          { label: 'apple' },
+          { label: 'banana' },
+          { label: 'pear' }
+        ]}
+        renderItem={(item, isHighlighted) =>
+          <div style={{ background: isHighlighted ? 'lightgray' : 'white' }}>
+            {item.label}
+          </div>
+        }
+        value=''
+        onChange={(e) => value = e.target.value}
+        onSelect={(val) => value = val}
+      />
       <input ref="searchbar" placeholder="Search..." style={{ height: "1.5rem", width: "20rem" }} />
       <button type="submit">Search</button>
       </form>
