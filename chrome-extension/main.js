1eb0b630116a70daf291b2c20750d8543f3fedc3
Connor J. Toth <tothconnor@gmail.com>
2018-10-13 01:05:20 -0500

    Updated manifrest; geolocation only able to work on secure protocols

--- chrome-extension/main.js
+++ chrome-extension/main.js
@@ -2,3 +2,18 @@
 // DURIAN INC.
 ////////////////////////////////////////////////////////////////////////////////
 
+
+
+/* Gets the user's current location */
+function getUserCurrentLocation() {
+
+  return Coordinates(5,2);
+}
+
+/* Calls backend with current location of the user
+ * Returns a list of locations/events to display w/relevant information */
+function getListWithLocation(coordinates) {
+
+  return Array();
+}
+
