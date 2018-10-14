448fc819b1ce43ac258c292b04e665b59dc7137a
Connor J. Toth <tothconnor@gmail.com>
2018-10-13 19:53:19 -0500

    BROEK!

--- react-app/src/components/SearchbarContainer.js
+++ react-app/src/components/SearchbarContainer.js
@@ -3,34 +3,80 @@ import Autocomplete from 'react-autocomplete'
 //import GoogleMapsLoader from "google-maps";
 //import styled from "styled-components";
 
-/*const Map = styled.div`
-  height: 100%;
-  width: 100%;
-`;
-*/
+
+
+/* Make a request to the Google Place API with our query */
+function requestPlaceApi(query) {
+  var url = 'https://maps.googleapis.com/maps/api/place/autocomplete/json?'
+      + 'input=' + escape(query) + '&key=AIzaSyCAgU40OXQVFZ5azzF13WtS20OM8pGFCH4';
+  console.log(13, url);
+  return fetch(url).then((response) => {
+    console.log(15, response.json());
+    return response.json()['predictions'];
+  });
+}
+
 class SearchbarContainer extends Component {
 
+  state = {
+    value: '',
+    suggestedValues: []
+  }
+
+  currentPromise = null;
   render() {
-    return <form>
-      <Autocomplete
-        getItemValue={(item) => item.label}
-        items={[
-          { label: 'apple' },
-          { label: 'banana' },
-          { label: 'pear' }
-        ]}
-        renderItem={(item, isHighlighted) =>
-          <div style={{ background: isHighlighted ? 'lightgray' : 'white' }}>
-            {item.label}
-          </div>
-        }
-        value=''
-        onChange={(e) => value = e.target.value}
-        onSelect={(val) => value = val}
-      />
-      <input ref="searchbar" placeholder="Search..." style={{ height: "1.5rem", width: "20rem" }} />
-      <button type="submit">Search</button>
+
+    console.log(33, this);
+    console.log(34, this.currentPromise);
+    return (
+      <form>
+        <label htmlFor="search-autocomplete">Search</label>
+        <Autocomplete
+          inputProps={{ id: 'search-autocomplete' }}
+          wrapperStyle={{ position: 'absolute',
+                          display: 'inline-block',
+                          zIndex: 5
+          }}
+          value={this.state.value}
+          items={this.state.suggestedValues}
+          getItemValue={(item) => item.description}
+          
+          // update the value and the suggested values
+          onSelect={(value, item) => {
+            this.setState({ value, suggestedValues: [ item ] });
+          }}
+          
+          // update the state and forget about previous changes
+          onChange={(event, value) => {
+            this.setState({ value })
+            console.log(this);
+            console.log(this.currentPromise);
+            this.currentPromise = null;
+
+            // display the new list of items
+            if (value.length >= 3) {
+              this.currentPromise = requestPlaceApi(value)
+                .then((itemsList) => {
+                  this.setState({ suggestedValues: itemsList })
+                });
+            }
+            else {
+              this.currentPromise = null;
+              this.setState({ suggestedValues: []})
+            }
+          }}
+          renderMenu={children => (
+            <div className="menu">
+              {children}
+            </div>
+          )}
+          renderItem={(item, isHighlighted) => (
+            <div className={`item ${isHighlighted ? 'item-highlighted' : ''}`}
+              key={item.abbr}>{item.name}</div>
+          )}
+        />
       </form>
+    )
   }
 }
 
