88f363ade1393093d85ce4a2aed019dec751f192
Derek Hanger <derekhanger@Dereks-MacBook-Pro.local>
2018-10-13 19:23:16 -0500

    stuff

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 import "./App.css";
-import { MapContainer, CheckboxContainer } from "./components";
+import { MapContainer, CheckboxContainer, FilterBox } from "./components";
 
 const AppWrap = styled.div`
   height: 100%;
@@ -12,6 +12,7 @@ class App extends Component {
   render() {
     return (
       <AppWrap>
+        <FilterBox />
         <CheckboxContainer />
         <MapContainer />
       </AppWrap>
