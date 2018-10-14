b11dbfda9435130799ad7c50b8103a77637da234
Jennifer Coleman <jkc266@mst.edu>
2018-10-13 23:27:18 -0500

    Restructured Database

--- api/app/database/db.sql
+++ api/app/database/db.sql
@@ -1,12 +1,44 @@
 CREATE TABLE Markers(
-    formatted_address VARCHAR(255) NOT NULL,
-    formatted_phone VARCHAR(80),
-    id VARCHAR(255) NOT NULL,
+    m_address VARCHAR(255) NOT NULL,
+    m_phone VARCHAR(80),
+    m_id VARCHAR(255) NOT NULL,
     m_name VARCHAR(255) NOT NULL,
     lat FLOAT(10, 6) NOT NULL,
     lng FLOAT(10, 6) NOT NULL,
     m_type CHAR(1) NOT NULL,
     PRIMARY KEY(id)
+);
+
+
+CREATE TABLE Ratings(
+    r_id VARCHAR(255) NOT NULL,
+    m_id VARCHAR(255) NOT NULL,
+    /* --- PRIVACY RATING --- */
+    p_rating int CHECK(privacy_rating > -1 AND privacy_rating < 6),
+    /* --- COMMUNICATION THROUGH BARRIERS RATING --- */
+    c_rating int CHECK(privacy_rating > -1 AND privacy_rating < 6),
+    FOREIGN KEY(m_id) REFERENCES Markers,
+    PRIMARY KEY(r_id),
+);
+
+/* --- EVERY ESTABLISHMENT WILL HAVE A DIFFERENT LITERACY RATING
+        FOR EACH LANGUAGE --- */
+CREATE TABLE Literacy(
+    lang varchar(50) NOT NULL,
+    id VARCHAR(255) NOT NULL,
+    /* --- LITERACY IN LANGUAGE RATING --- */
+    l_rating int CHECK(privacy_rating > -1 AND privacy_rating < 6),
+    FOREIGN KEY(id) REFERENCES Markers,
+    PRIMARY KEY(lang)
+);
+
+CREATE TABLE Events(
+    e_id = varchar(255) NOT NULL,
+    e_name = varchar(120) DEFAULT 'Event',
+    lat FLOAT(10, 6) NOT NULL,
+    lng FLOAT(10, 6) NOT NULL,
+    e_type = CHAR CHECK(e_type = 'E' OR e_type = 'J'),
+    FOREIGN KEY(lat,lng) REFERENCES Markers
 );
 
 INSERT INTO `Markers` (`id`, `m_name`, `formatted_address`, `lat`, `lng`, `m_type`) VALUES ('1', 'Love.Fish', '580 Darling Street, Rozelle, NSW', '-33.861034', '151.171936', 'L');
